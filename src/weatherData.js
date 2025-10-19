11b36cd23c911ed4ee4cfe79093d9799e580b3a5
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-10-19 12:16:25 -0400

    Wrote process weather data function

--- src/weatherData.js
+++ src/weatherData.js
@@ -15,3 +15,26 @@ export async function getWeatherData(location) {
     console.log(`There was an error: ${error}`);
   }
 }
+
+export async function processWeatherData(weatherData, location) {
+  const unprocessedWeatherData = await weatherData(location);
+
+  const processedWeatherData = {};
+  processedWeatherData.location = unprocessedWeatherData.resolvedAddress;
+  processedWeatherData.conditions =
+    unprocessedWeatherData.currentConditions.conditions;
+  processedWeatherData.temperature =
+    unprocessedWeatherData.currentConditions.temp;
+  processedWeatherData.feelsLike =
+    unprocessedWeatherData.currentConditions.feelslike;
+  processedWeatherData.humidity =
+    unprocessedWeatherData.currentConditions.humidity;
+  processedWeatherData.uvIndex =
+    unprocessedWeatherData.currentConditions.uvindex;
+  processedWeatherData.windspeed =
+    unprocessedWeatherData.currentConditions.windspeed;
+  processedWeatherData.description = unprocessedWeatherData.description;
+  processedWeatherData.icon = unprocessedWeatherData.currentConditions.icon;
+
+  return processedWeatherData;
+}
