6d974708baaac2afcbda206b8adabbd58f63bee0
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-10-19 12:16:13 -0400

    Tested weather function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // Weather app JS index
 
 import "./style.css";
-import { getWeatherData } from "./weatherData.js";
+import { getWeatherData, processWeatherData } from "./weatherData.js";
+
+console.log(processWeatherData(getWeatherData, "11201"));
