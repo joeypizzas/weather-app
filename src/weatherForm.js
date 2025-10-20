a93203cf8a2065e572147e20d213e3e1bb3aa6e7
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-10-20 19:39:18 -0400

    Added custom form validation

--- src/weatherForm.js
+++ src/weatherForm.js
@@ -1,9 +1,14 @@
 // Weather form view JS
 
 export function initFormListeners() {
+  const form = document.querySelector("form");
   const input = document.querySelector("input");
   const button = document.querySelector("button");
   const searchSVG = document.querySelector(".search-svg");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
 
   input.addEventListener("mouseover", () => {
     input.classList.remove("input-color");
@@ -13,6 +18,18 @@ export function initFormListeners() {
     input.classList.remove("input-color-hover");
     input.classList.add("input-color");
   });
+  input.addEventListener("blur", () => {
+    input.setCustomValidity("");
+    if (!input.checkValidity()) {
+      input.setCustomValidity("Please enter a city or zip code.");
+      input.reportValidity();
+    } else {
+      input.setCustomValidity("");
+    }
+  });
+  input.addEventListener("input", () => {
+    input.setCustomValidity("");
+  });
 
   button.addEventListener("mouseover", () => {
     searchSVG.classList.remove("search-svg-color");
@@ -21,5 +38,18 @@ export function initFormListeners() {
   button.addEventListener("mouseout", () => {
     searchSVG.classList.remove("search-svg-color-hover");
     searchSVG.classList.add("search-svg-color");
+  });
+  button.addEventListener("mousedown", () => {
+    searchSVG.classList.remove("search-svg-color-hover");
+    searchSVG.classList.add("search-svg-color-click");
+  });
+  button.addEventListener("mouseup", () => {
+    searchSVG.classList.remove("search-svg-color-click");
+    searchSVG.classList.add("search-svg-color-hover");
+    if (form.checkValidity()) {
+      input.value = "";
+    } else {
+      form.reportValidity();
+    }
   });
 }
